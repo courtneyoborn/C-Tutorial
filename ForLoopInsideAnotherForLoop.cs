046a146d600afe638fa09d4eb1c4a3a6b098933e
Courtney <courtney.oborn@mcdermottgroup.co.uk>
2018-07-09 10:16:01 +0100

    Added for loop class

---
+++ ForLoopInsideAnotherForLoop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UdemyTutorial
+{
+    class ForLoopInsideAnotherForLoop
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            string hashtag = "#";
+            for (int i = 0; i < count; i++)
+            {
+
+
+                for (int j = 0; j < i; ++j)
+                {
+                    Console.Write(hashtag);
+                }
+                Console.WriteLine(hashtag);
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
--- Program.cs
+++ Program.cs
@@ -5,24 +5,12 @@ using WhileLoop;
 
 namespace UdemyTutorial
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
 
-            int count = int.Parse(Console.ReadLine());
-            string hashtag = "#";
-            for (int i = 0; i < count; i++)
-            {
-                string answer = hashtag + "#";
-                for (int j=0; j <= i; ++j)
-                {
-                    
-                    Console.WriteLine(answer);
-                }
-                
-
-            }
+          
             
 
 

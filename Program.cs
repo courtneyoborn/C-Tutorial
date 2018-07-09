9d01101e33c1b973133e3a51f902dcf10b4d2545
Courtney <courtney.oborn@mcdermottgroup.co.uk>
2018-07-09 16:17:53 +0100

    first attempt at antivirus CodEasy challenge

--- Program.cs
+++ Program.cs
@@ -9,8 +9,41 @@ namespace UdemyTutorial
     {
         static void Main(string[] args)
         {
+            int number = 0;
+            bool unhide = true;
+            bool hide = false;
+            while(Console.ReadLine() == "scan")
+            {
+                if (Console.ReadLine() == "scan")
+                {
+                    number++;
+                    Console.WriteLine($"scanning file {number}");
+                }
 
-            
+                else if (Console.ReadLine() == "hide")
+                {
+                    unhide = false;
+                    hide = true;
+                }  
+            }
+
+            while (hide)
+            {
+                if (Console.ReadLine() == "hide")
+                {
+                    if (Console.ReadLine() == "scan")
+                    {
+                        Console.WriteLine("can't scan files for viruses");
+                    }
+                            
+                }
+            }
+
+            if (Console.ReadLine() == "game over")
+            {
+                Console.WriteLine("run");
+            }
+
         }
     }
 }
